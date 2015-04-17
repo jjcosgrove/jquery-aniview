11934619b97c28db7d9813bc491f66137970ea4b
Jonathan James Cosgrove <jjcosgrove.inbox@gmail.com>
2015-04-17 11:18:21 +0100

    Added syntax highlighting

--- js/script.js
+++ js/script.js
@@ -3,4 +3,8 @@ $(document).ready(function() {
         animateThreshold: 100
     };
     $('.aniview').AniView(options);
+
+    $('.code').each(function(i, block) {
+        hljs.highlightBlock(block);
+    });
 });
